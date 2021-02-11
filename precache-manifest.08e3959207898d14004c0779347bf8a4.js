9bb7528b2ced2d4712a047faf0c3efbe1e33b5b8
CEven98 <doomfire98@gmail.com>
2021-02-10 21:58:05 -0600

    Updates

---
+++ precache-manifest.08e3959207898d14004c0779347bf8a4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97b7d7d1377d1c70523dd1e2454a8a6",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "9a5d655c750d1de0a857",
+    "url": "/React-Portfolio/static/css/main.dc3c21be.chunk.css"
+  },
+  {
+    "revision": "2f2d953e8778055dd095",
+    "url": "/React-Portfolio/static/js/2.896ac9c3.chunk.js"
+  },
+  {
+    "revision": "a1bd720283e6a590c3ab3794c1f81d89",
+    "url": "/React-Portfolio/static/js/2.896ac9c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a5d655c750d1de0a857",
+    "url": "/React-Portfolio/static/js/main.b8b3c463.chunk.js"
+  },
+  {
+    "revision": "553b223697e93b4dcf93",
+    "url": "/React-Portfolio/static/js/runtime-main.7fdaa00c.js"
+  },
+  {
+    "revision": "ceefafc45a546e52ddfd13cf414296e2",
+    "url": "/React-Portfolio/static/media/MERN-logo.ceefafc4.png"
+  },
+  {
+    "revision": "d27d6d7bda81b364e1a19664e7ee1b10",
+    "url": "/React-Portfolio/static/media/RecipeGen.d27d6d7b.png"
+  },
+  {
+    "revision": "96f038b128363833d10d6745f827b236",
+    "url": "/React-Portfolio/static/media/TPF.96f038b1.gif"
+  },
+  {
+    "revision": "27f37cee2ab817ef72af920b562dac82",
+    "url": "/React-Portfolio/static/media/note-taker.27f37cee.png"
+  },
+  {
+    "revision": "17b29196a5fb31e39d52838ea8b0a09d",
+    "url": "/React-Portfolio/static/media/red_bag.17b29196.png"
+  },
+  {
+    "revision": "842c50644d4b7480fef8aa4f682a1376",
+    "url": "/React-Portfolio/static/media/resume.842c5064.jpg"
+  },
+  {
+    "revision": "25fae8f053265d481b97856be7872125",
+    "url": "/React-Portfolio/static/media/work-scheduler.25fae8f0.png"
+  }
+]);
